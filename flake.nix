c4531eb09a24e7189f90fb5f322360845c8391c9
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-16 17:08:12 +0300

    add: flake.nix

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Nix Flake for NetCoMi R package";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    SpiecEasi.url = "github:artur-sannikov/SpiecEasi/nix-flakes";
+    SPRING.url = "github:artur-sannikov/SPRING/nix-flakes";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      SpiecEasi,
+      SPRING,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        SpiecEasiPkg = SpiecEasi.packages.${system}.default;
+        SPRINGPkg = SPRING.packages.${system}.default;
+        NetCoMiPkg = pkgs.rPackages.buildRPackage {
+          name = "NetCoMi";
+          src = self;
+          nativeBuildInputs =
+            with pkgs.rPackages;
+            [
+              Biobase
+              corrplot
+              doSNOW
+              fdrtool
+              filematrix
+              foreach
+              gtools
+              huge
+              igraph
+              MASS
+              Matrix
+              mixedCCA
+              orca
+              phyloseq
+              pulsar
+              qgraph
+              RColorBrewer
+              Rdpack
+              rlang
+              vegan
+              WGCNA
+            ]
+            ++ [
+              SpiecEasiPkg
+              SPRINGPkg
+            ];
+        };
+      in
+      {
+        packages.default = NetCoMiPkg;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ NetCoMiPkg ];
+        };
+      }
+    );
+}
